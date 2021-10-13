f92232a372a71f69057702a28f2c453158bd71d8
FarizieFar <90998221+FarizieFar@users.noreply.github.com>
2021-10-13 17:57:52 +0700

    AlfariziAbd

---
+++ Segitiga.java
@@ -0,0 +1,16 @@
+	import java.util.Scanner;
+
+		public class Segitiga{
+
+			public static void main(String[] args){
+				Scanner sc = new Scanner(System.in);
+				int alas, tinggi;
+				float luas;
+				System.out.println("Masukkan alas: ");
+				alas = sc.nextInt();
+				System.out.println("Masukkan tinggi: ");
+				tinggi = sc.nextInt();
+				luas = alas * tinggi / 2;
+				System.out.println("Luas segitiga: " + luas);
+}
+}
